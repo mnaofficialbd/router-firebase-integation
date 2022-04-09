1eed3f3979d7fbe4d79b60e953ce46fa9f60e420
mnaofficial <mna.official92@gmail.com>
2022-04-09 18:40:54 +0600

    Add signout auth

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,7 +4,7 @@ import useFirebase from '../../hooks/useFirebase';
 import "./Header.css"
 
 const Header = () => {
-    const { user } = useFirebase()
+    const { user,handleSignOut } = useFirebase()
     return (
         <div className="header">
             <nav>
@@ -13,8 +13,13 @@ const Header = () => {
                 <Link to="/orders">Orders</Link>
                 <Link to="/reviews">Reviews</Link>
                 <Link to="/register">Register</Link>
-                {user.uid ? (<button>Signout</button>) :
-                    (<Link to="/login">Log In</Link>)}
+                <span>{user?.displayName && user.displayName}</span>
+                {
+                user?.uid ? (
+                <button onClick={handleSignOut}>Sign out</button>
+                ) :
+                    (<Link to="/login">Log In</Link>)
+                    }
             </nav>
         </div>
     );
--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,5 +1,5 @@
-import {  useState } from 'react';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useEffect, useState } from 'react';
+import { getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOut } from "firebase/auth";
 import app from "../firebase.init"
 
 const auth = getAuth(app);
@@ -7,24 +7,36 @@ const googleProvider = new GoogleAuthProvider();
 
 
 const useFirebase = () => {
-    const [user,setUser]=useState({});
+  const [user, setUser] = useState({});
 
 
-const signInWithGoogle=()=>{
+  const signInWithGoogle = () => {
     signInWithPopup(auth, googleProvider)
-    .then((result) => {
-      const user = result.user;
-      console.log(user);
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        setUser(user)
+      }).catch((error) => {
+        const errorMessage = error.message;
+        const email = error.email;
+      });
+  }
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+      })
+  }
+
+  useEffect(() => {
+    onAuthStateChanged(auth, user => {
       setUser(user)
-    }).catch((error) => {
-      const errorMessage = error.message;
-      const email = error.email;
-    });
-}
+    })
+  }, [])
 
 
-    // return [user,setUser]
-    return {user,signInWithGoogle}
+  // return [user,setUser]
+  return { user, signInWithGoogle,handleSignOut }
 };
 
 export default useFirebase;
