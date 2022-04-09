942c36a80d4676966342100c25849a3141fcab94
mnaofficial <mna.official92@gmail.com>
2022-04-09 20:10:04 +0600

    RequireAuth add

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import './App.css';
-import {  Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './Components/Home/Home';
 import Login from './Components/Login/Login';
 import Header from './Components/Header/Header';
 import Register from './Components/Register/Register';
 import Products from './Components/Products/Products';
+import Orders from './Components/Orders/Orders';
+import RequireAuth from './Components/RequireAuth/RequireAuth';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/home' element={<Home/>}></Route>
-        <Route path='/login' element={<Login/>}></Route>
-        <Route path='/register' element={<Register/>}/>
-        <Route path='/products' element={<Products/>}></Route>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/home' element={<Home />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/register' element={<Register />} />
+        <Route path='/products' element={<Products />}></Route>
+        <Route path='/orders' element={
+          <RequireAuth>
+            <Orders />
+          </RequireAuth>
+        }></Route>
       </Routes>
     </div>
   );
